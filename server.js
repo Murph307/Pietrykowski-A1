fc0f20786ec8e8741a823d38e752ff307e2e07db
Andrew Pietrykowski <ap2315@mcla.edu>
2019-02-06 18:42:23 +0000

    submission

--- server.js
+++ server.js
@@ -16,21 +16,81 @@ function requestHandler(req, res)
     
     res.writeHead(200, {'Content-Type': 'text/html'});
     
-    if (query['cmd'] == 'add')
+    if (query['cmd'] == 'repeat')
+    {
+      console.log("Handling a request");
+      console.log(query);
+      var word = query['word'];
+      var x = word.length;
+      for (var i in query['word'])
+      {
+          for(i = 1; i <= word.length; i++)
+          {
+               res.write('<pre>'+word+'</pre>');
+          }
+          break;
+      }
+      
+      res.end('');
+    }
+    
+    
+    if (query['cmd'] == 'dotted')
+    {
+      console.log("Handling a request");
+      console.log(query);
+      var word1 = query['word1'];
+      var word2 = query['word2'];
+      var x1 = word1.length;
+      var x2 = word2.length;
+      var dots = 30 - (x1 + x2);
+
+      res.write('<pre>' + word1);
+      for(i = 0; i < dots; i++)
+      {
+          res.write(".");
+      }
+      res.write(word2 + '</pre>');
+    }
+    
+    
+    if (query['cmd'] == 'stats')
     {
       console.log("Handling a request");
       console.log(query);
       var sum = 0;
-      for (var i in query['num'])
+      var min = 0;
+      var max = 0;
+      var average = 0;
+      for (var i in query['grades'])
       {
-        sum = sum + parseInt(query['num'][i]);
+        sum = sum + parseInt(query['grades'][i]);
+        average = sum / query['grades'].length;
       }
+      min = query['grades'][0];
+      max = query['grades'][0];
+      for(var i = 0; i < query['grades'].length; i++)
+      {
+        if(i + 1 <= query['grades'].length && query['grades'][i] < query['grades'][i + 1] )
+        {
+          min = query['grades'][i];
+        }
+      }
+      for(var i = 0; i < query['grades'].length; i++)
+      {
+        if(i - 1 >= 0 && query['grades'][i] > query['grades'][i - 1] )
+        {
+          max = query['grades'][i];
+        }
+      }
+
+      res.write('<pre>Ave: ' + average + ' Min: '+ min + ' Max: '+ max +'</pre>');
       
-      res.write('<pre>'+sum+'</pre>');
       res.end('');
     }
     else
     {
       res.end('');
     }
+    
 }
